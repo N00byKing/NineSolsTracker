66ab6d7b8f26b4dd7fd03737af356d942098b5ea
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-08-11 11:19:05 +0200

    disable modding API by default

--- Source/ExampleMod.cs
+++ Source/ExampleMod.cs
@@ -1,11 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using NineSolsAPI;
 using UnityEngine;
+// using NineSolsAPI;
 
 namespace ExampleMod;
 
-[BepInDependency(NineSolsAPICore.PluginGUID)]
+// [BepInDependency(NineSolsAPICore.PluginGUID)] if you want to use the API
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class ExampleMod : BaseUnityPlugin {
     // https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/4_configuration.html
@@ -19,14 +19,21 @@ public class ExampleMod : BaseUnityPlugin {
         enableSomethingConfig = Config.Bind("General.Something", "Enable", true, "Enable the thing");
         somethingKeyboardShortcut = Config.Bind("General.Something", "Shortcut",
             new KeyboardShortcut(KeyCode.Q, KeyCode.LeftShift), "Shortcut to execute");
-        KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
+
+        // If you want to use the modding API, enable it in the .csproj.
+        // It provides utilities like the KeybindManager, utilities for Instantiating objects including the 
+        // NineSols lifecycle hooks, displaying toast messages and preloading objects from other scenes.
+        // If you do use the API make sure do download the [NineSolsAPI.dll](https://github.com/nine-sols-modding/NineSolsAPI/releases/)
+        // and put it in BepInEx/plugins/.
+
+        // KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
 
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 
     private void TestMethod() {
         if (!enableSomethingConfig.Value) return;
-        ToastManager.Toast("Shortcut activated");
+        // ToastManager.Toast("Shortcut activated");
     }
 
     private void OnDestroy() {
