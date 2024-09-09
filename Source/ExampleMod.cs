295a9c0eef598e14326dc3ed37ba83413660533a
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-09-09 18:12:01 +0200

    reenable modding API and add thunderstore configuration

--- Source/ExampleMod.cs
+++ Source/ExampleMod.cs
@@ -1,11 +1,12 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
 using UnityEngine;
-// using NineSolsAPI;
 
 namespace ExampleMod;
 
-// [BepInDependency(NineSolsAPICore.PluginGUID)] if you want to use the API
+[BepInDependency(NineSolsAPICore.PluginGUID)]
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class ExampleMod : BaseUnityPlugin {
     // https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/4_configuration.html
@@ -18,22 +19,31 @@ public class ExampleMod : BaseUnityPlugin {
 
         enableSomethingConfig = Config.Bind("General.Something", "Enable", true, "Enable the thing");
         somethingKeyboardShortcut = Config.Bind("General.Something", "Shortcut",
-            new KeyboardShortcut(KeyCode.Q, KeyCode.LeftShift), "Shortcut to execute");
+            new KeyboardShortcut(KeyCode.H, KeyCode.LeftControl), "Shortcut to execute");
 
-        // If you want to use the modding API, enable it in the .csproj.
+        // Usage of the modding API is entirely optional.
         // It provides utilities like the KeybindManager, utilities for Instantiating objects including the 
         // NineSols lifecycle hooks, displaying toast messages and preloading objects from other scenes.
-        // If you do use the API make sure do download the [NineSolsAPI.dll](https://github.com/nine-sols-modding/NineSolsAPI/releases/)
-        // and put it in BepInEx/plugins/.
+        // If you do use the API make sure do have it installed when running your mod, and keep the dependency in the
+        // thunderstore.toml.
 
-        // KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
+        KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
 
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 
+    // Some fields are private and need to be accessed via reflection.
+    // You can do this with `typeof(Player).GetField("_hasHat", BindingFlags.Instance|BindingFlags.NonPublic).GetValue(Player.i)`
+    // or using harmony access tools:
+    private static readonly AccessTools.FieldRef<Player, bool>
+        PlayerHasHat = AccessTools.FieldRefAccess<Player, bool>("_hasHat");
+    
     private void TestMethod() {
         if (!enableSomethingConfig.Value) return;
-        // ToastManager.Toast("Shortcut activated");
+        ToastManager.Toast("Shortcut activated");
+
+        var hasHat = PlayerHasHat.Invoke(Player.i);
+        Player.i.SetHasHat(!hasHat);
     }
 
     private void OnDestroy() {
