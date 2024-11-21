b7147e944d892aacf3093fda208602b845715f39
N00byKing <N00byKing@hotmail.de>
2024-11-21 23:46:36 +0100

    Add more connectors

--- Source/Connectors.cs
+++ Source/Connectors.cs
@@ -3,22 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 static class Connectors {
-    private static readonly Dictionary<String, Vector3> ConnectorDictA1 = new() {
+    private static readonly Dictionary<String, Vector3> ConnectorDictA1S1 = new() {
         {"A1_S1_A1_S2", new Vector3(-1550, -3900)}
     };
-    private static readonly Dictionary<String, HashSet<InterestPointData>?> ConnectorSetMap = new() {
-        {"Minimap_A1_S1_HumanDisposal_Final_Setting", null}
+    private static readonly Dictionary<String, Vector3> ConnectorDictA1S2 = new() {
+        {"A1_S2_A1_S1", new Vector3(-1500, -3315)},
+        {"A1_S2_A1_S3", new Vector3(-1650, -4580)},
+        {"A1_S2_A2_S6", new Vector3(2820, -4430)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA1S3 = new() {
+        {"A1_S3_A1_S2", new Vector3(-2600, -1010)},
+        {"A1_S3_A2_S3", new Vector3(-6330, -1150)},
+        {"A1_S3_A6_S1", new Vector3(-6900, 50)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA2S1 = new() {
+        {"A2_S1_A2_S3", new Vector3(-6170, -1800)},
+        {"A2_S1_A2_S2", new Vector3(1380, -1800)},
+        {"A2_S1_A2_S5", new Vector3(-2370, -1880)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA2S2 = new() {
+        {"A2_S2_A2_S1", new Vector3(-6950, -1600)},
+        {"A2_S2_A2_S6", new Vector3(-2070, -1880)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA2S3 = new() {
+        {"A2_S3_A1_S3", new Vector3(-7280, -1620)},
+        {"A2_S3_A2_S1", new Vector3(-2350, -1180)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA2S5 = new() {
+        {"A2_S5_A2_S1", new Vector3(-6900, -2270)},
+    };
+    private static readonly Dictionary<String, Vector3> ConnectorDictA2S6 = new() {
+        {"A2_S6_AG_S1", new Vector3(5500, -4350)},
+        {"A2_S6_A0_S10", new Vector3(3000, -6768)},
+        {"A2_S6_A1_S2", new Vector3(4500, -8320)},
+        {"A2_S6_A2_S2", new Vector3(7800, -8220)},
+        {"A2_S6_A11_S1", new Vector3(7480, -7400)},
+    };
+    private static readonly Dictionary<String, (HashSet<InterestPointData>? IPDs, Dictionary<String, Vector3> ConnDict)> ConnectorSetMap = new() {
+        {"Minimap_A1_S1_HumanDisposal_Final_Setting", (null, ConnectorDictA1S1)},
+        {"Minimap_A1_S2_ConnectionToElevator_Final_Setting", (null, ConnectorDictA1S2)},
+        {"Minimap_A1_S3_InnerHumanDisposal_Final_Setting", (null, ConnectorDictA1S3)},
+        {"Minimap_A2_S1_ReactorMiddle_Final_Setting", (null, ConnectorDictA2S1)},
+        {"Minimap_A2_S2_ReactorRight_Final_Setting", (null, ConnectorDictA2S2)},
+        {"Minimap_A2_S3_ReactorLeft_Final_Setting", (null, ConnectorDictA2S3)},
+        {"Minimap_A2_S5_BossHorseman_Final_Setting", (null, ConnectorDictA2S5)},
+        {"Minimap_A2_S6_LogisticCenter_Final_Setting", (null, ConnectorDictA2S6)},
+        
     };
 
     public static HashSet<InterestPointData> GetConnections(String region) {
         if (!ConnectorSetMap.ContainsKey(region)) return [];
-        if (ConnectorSetMap[region] != null) return ConnectorSetMap[region]!;
+        if (ConnectorSetMap[region].IPDs != null) return ConnectorSetMap[region].IPDs!;
 
         // Empty, need to initialize
-        ConnectorSetMap[region] = [];
-        foreach ((String conn, Vector3 loc) in ConnectorDictA1) {
-            ConnectorSetMap[region]!.Add(InterestDataMapping.CreateIPD(conn, loc));
+        HashSet<InterestPointData> IPDs = [];
+        foreach ((String conn, Vector3 loc) in ConnectorSetMap[region].ConnDict) {
+            IPDs.Add(InterestDataMapping.CreateIPD(conn, loc));
         }
-        return ConnectorSetMap[region]!;
+        ConnectorSetMap[region] = (IPDs, ConnectorSetMap[region].ConnDict);
+        return ConnectorSetMap[region].IPDs!;
     }
 }
--- Source/InterestDataMapping.cs
+++ Source/InterestDataMapping.cs
@@ -44,12 +44,53 @@ static class InterestDataMapping {
 
     private static readonly Dictionary<String, (String name, IPDKind kind)> ToHumanReadable = new()
     {
+        // AG_S1: New Kunlun Central Hall
+
+        // A0_S10: Galactic Dock
+
         // A1_S1: Apeman Facility (Monitoring)
         {"A1_S1_HumanDisposal_Final_MoneyCrateFlag27cfe3ad-4c1d-4d51-bcee-80e5c7bebf24", ("Apeman Facility (Monitoring) - Jin Chest near first big enemy", IPDKind.MoneyCrate)},
         {"A1_S1_HumanDisposal_Final_MoneyCrateFlag71961019-5ce4-4bcb-834b-c5b84360eb4a", ("Apeman Facility (Monitoring) - Jin Chest in Tunnels", IPDKind.MoneyCrate)},
         {"A1_S1_HumanDisposal_Final_[Variable] Pickedb7eb8443-41b9-42cc-b809-0ff7dd487e96", ("Apeman Facility (Monitoring) - Item from Meat Machine", IPDKind.DropItem)},
         {"A1_S1_HumanDisposal_Final_[Variable] Pickedf78b77c1-de42-4a36-87ce-255804e9826d", ("Apeman Facility (Monitoring) - Encyclopedia Item", IPDKind.Encyclopedia)},
-        {"A1_S1_A1_S2", ("Connector Apeman Facility (Monitoring) to Apeman Facility (Elevator)", IPDKind.Connector)}
+        {"A1_S1_A1_S2", ("Apeman Facility (Monitoring) to Apeman Facility (Elevator)", IPDKind.Connector)},
+
+        // A1_S2: Apeman Facility (Elevator)
+        {"A1_S2_A1_S1", ("Apeman Facility (Elevator) to Apeman Facility (Monitoring)", IPDKind.Connector)},
+        {"A1_S2_A1_S3", ("Apeman Facility (Elevator) to Apeman Facility (Depths)", IPDKind.Connector)},
+        {"A1_S2_A2_S6", ("Apeman Facility (Elevator) to Central Transport Hub", IPDKind.Connector)},
+
+        // A1_S3: Apeman Facility (Depths)
+        {"A1_S3_A1_S2", ("Apeman Facility (Depths) to Apeman Facility (Elevator)", IPDKind.Connector)},
+        {"A1_S3_A2_S3", ("Apeman Facility (Depths) to Power Reservoir (West)", IPDKind.Connector)},
+        {"A1_S3_A6_S1", ("Apeman Facility (Depths) to Factory (Underground)", IPDKind.Connector)},
+
+        // A2_S1: Power Reservoir (Central)
+        {"A2_S1_A2_S2", ("Power Reservoir (Central) to Power Reservoir (East)", IPDKind.Connector)},
+        {"A2_S1_A2_S3", ("Power Reservoir (Central) to Power Reservoir (West)", IPDKind.Connector)},
+        {"A2_S1_A2_S5", ("Power Reservoir (Central) to Radiant Pagoda", IPDKind.Connector)},
+
+        // A2_S2: Power Reservoir (East)
+        {"A2_S2_A2_S1", ("Power Reservoir (East) to Power Reservoir (Central)", IPDKind.Connector)},
+        {"A2_S2_A2_S6", ("Power Reservoir (East) to Central Transport Hub", IPDKind.Connector)},
+
+        // A2_S3: Power Reservoir (West)
+        {"A2_S3_A1_S3", ("Power Reservoir (West) to Apeman Facility (Depths)", IPDKind.Connector)},
+        {"A2_S3_A2_S1", ("Power Reservoir (West) to Power Reservoir (Central)", IPDKind.Connector)},
+
+        // A2_S5: Radiant Pagoda
+        {"A2_S5_A2_S1", ("Radiant Pagoda to Power Reservoir (Central)", IPDKind.Connector)},
+
+        // A2_S6: Central Transport Hub
+        {"A2_S6_AG_S1", ("Central Transport Hub to New Kunlun Central Hall", IPDKind.Connector)},
+        {"A2_S6_A0_S10", ("Central Transport Hub to Galactic Dock", IPDKind.Connector)},
+        {"A2_S6_A1_S2", ("Central Transport Hub to Apeman Facility (Elevator)", IPDKind.Connector)},
+        {"A2_S6_A2_S2", ("Central Transport Hub to Power Reservoir (East)", IPDKind.Connector)},
+        {"A2_S6_A11_S1", ("Central Transport Hub to Tiandao Research Center", IPDKind.Connector)},
+
+        // A6_S1: Factory (Underground)
+
+        // A11_S1: Tiandao Research Center
     };
     public static (String name, IPDKind kind) GetHumanReadable(InterestPointData IPD) {
         if (ToHumanReadable.ContainsKey(IPD.name)) return ToHumanReadable[IPD.name];
